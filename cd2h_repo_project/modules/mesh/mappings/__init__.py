dd2679f84faf9714350873eadb1993fe1f98b036
fenekku <fenekku@fenekku.com>
2019-04-30 11:19:24 -0500

    Copyright files correctly

--- cd2h_repo_project/modules/mesh/mappings/__init__.py
+++ cd2h_repo_project/modules/mesh/mappings/__init__.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of Invenio.
-# Copyright (C) 2016-2018 CERN.
+# Copyright (C) 2018 - present NU,FSM,GHSL.
 #
-# Invenio is free software; you can redistribute it and/or modify it
+# menRva is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""OpenDefinition Elasticsearch mappings."""
+"""MeSH Elasticsearch mappings."""
--- cd2h_repo_project/modules/mesh/suggester.py
+++ cd2h_repo_project/modules/mesh/suggester.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2018 - present NU,FSM,GHSL.
+#
+# menRva is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
 """Term suggestion engine."""
 
 from elasticsearch_dsl import Search
